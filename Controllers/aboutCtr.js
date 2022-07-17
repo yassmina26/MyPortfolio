9f5f5f437a42613a42931b6d07ad1cd3d811b0ed
Yassmine Boukari <lyce.26avril@gmail.com>
2022-07-17 17:08:11 +0100

    Improvements :)

--- Controllers/aboutCtr.js
+++ Controllers/aboutCtr.js
@@ -1,24 +1,83 @@
+const aboutSchema = require("../models/aboutModel")
 
-exports.getAbout = (req,res)=>{
-    res.send('hello from getAbout')
+// get  about user
+exports.getAbout = async (req, res) => {
+    const about = await aboutSchema.find();
+    try {
+        res.json(about);
+    
+    }catch (error) {
+        res.status(500).json({ msg: "Server problem" })
+    }
 }
 
-exports.addAbout = (req,res)=> {
-    res.send('hello from post router')
+
+// add  about user
+exports.addAbout = async (req, res) => {
+
+    //Getting all the data that there is in the body 
+    const { about } = req.body;
+
+    try {   // we dont want the application to break
+        const newAbout = new aboutSchema({
+            about  // or we can do about:about or about:req.body.about
+        })
+        await newAbout.save(); //save the data
+        res.json(newAbout); // see the data that has been saved
+
+    } catch (error) {
+        res.status(500).json({ msg: 'Server problem' })
+    }
+
 }
 
-exports.getAboutId = (req,res)=> {
-    res.send('hello from specific  post router')
+
+// get specific about user by id
+exports.getAboutId = async (req, res) => {
+
+    try {
+        const about = await aboutSchema.findById(req.params.id);
+        res.json(about);
+
+    } catch (error) {
+        res.status(500).json({ msg: 'Server problem' })
+    }
+
+
 }
 
-exports.updateAboutId = (req,res)=> {
-    res.send('hello from updated  post router')
+
+// update specific  about user by id
+exports.updateAboutId = async (req, res) => {
+
+    try {
+        const { about } = req.body;
+        const newAbout = await aboutSchema.findByIdAndUpdate(req.params.id, {
+            about
+        });
+        let results = await newAbout.save();
+        await results;
+        res.json({ msg: "Item updated" })
+
+    } catch (error) {
+        res.status(500).json({ msg: 'Server problem' })
+    }
 }
 
-exports.delAboutId = (req,res)=> {
-    res.send('hello from delete  post router')
-}
 
+// delete  about user
+exports.delAboutId = async (req, res) => {
+
+    try {
+        const about = await aboutSchema.findByIdAndDelete(req.params.id)
+        about;
+        res.json({ msg: "Item deleted" })
+
+    } catch (error) {
+        res.status(500).json({ msg: 'Server problem' })
+    }
+
+}
 
 
 
--- routes/aboutRoute.js
+++ routes/aboutRoute.js
@@ -2,89 +2,25 @@ const { application } = require('express');
 const { async } = require('rxjs');
 
 const router = require('express').Router();
-//const {getAbout, getAboutId, updateAboutId, delAboutId, addAbout} = require('../Controllers/aboutCtr');
+const { getAbout, getAboutId, updateAboutId, delAboutId, addAbout } = require('../Controllers/aboutCtr');
 
-const aboutSchema = require('../models/aboutModels');
+//const aboutSchema = require('../models/aboutModels');
 
 //....about....in order to test this part, I do it on postman 
 
 // get request in order to get the about 
-router.get('/about',  async (req,res)=>{
-    
-    const about = await aboutSchema.find();
+router.get('/about', getAbout)
 
-    res.json(about);
-})
- 
 // add about user 
-router.post('/about', async (req,res)=> {
-
-        //Getting all the data that there is in the body 
-        const {about} = req.body;
-
-    try {   // we dont want the application to break
-        
-        const newAbout = new aboutSchema({
-            about  // or we can do about:about or about:req.body.about
-       
-        })
-
-        await newAbout.save(); //save the data
-        res.json(newAbout); // see the data that has been saved
-
-    } catch (error) {
-        res.status(500).json({msg:'Server problem'})
-    }
-    
-})
+router.post('/about', addAbout)
 
 // get specific user by ID
-router.get('/about/:id', async (req,res)=> {
-   
-    try {
-        const about = await aboutSchema.findById(req.params.id);
-        res.json(about);
-
-        
-    } catch (error) {
-
-        res.status(500).json({msg:'Server problem'})
-    }
-    
-
-})
+router.get('/about/:id', getAboutId)
 
 // update specific user by ID
-router.put('/about/update/:id', async (req,res)=> {
-    
-    try {
-        const {about} = req.body;
-        const newAbout = await aboutSchema.findByIdAndUpdate(req.params.id, {
-     
-         about
-     
-        });
-        let results = await newAbout.save();
-        await results;
-        res.json({msg:"Item updated"})
-        
-    } catch (error) {
-        res.status(500).json({msg:'Server problem'})
-    }
-})
+router.put('/about/update/:id', updateAboutId)
 
 // delete specific user by ID
-router.delete('/about/:id', async (req,res)=> {
-  
-   
-    try {
-        const about = await aboutSchema.findByIdAndDelete(req.params.id)
-        about;
-        res.json({msg:"Item deleted"})
-        
-    }catch (error) {
-        res.status(500).json({msg:'Server problem'})
-    }
+router.delete('/about/:id', delAboutId)
 
-})
 module.exports = router;
