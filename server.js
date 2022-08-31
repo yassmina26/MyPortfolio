9f76d3253309a16f105dac7d05d037194574a2a8
Yassmine Boukari <lyce.26avril@gmail.com>
2022-08-31 17:46:26 +0100

    up

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ mongoose.connect(URI, {
 // About route 
 app.use('/', require('./routes/aboutRoute'));
 // Certif route
-app.use('/', require('./routes/CertifRoute'));
+app.use('/', require('./routes/certifRoute'));
 // Project route
 app.use('/', require('./routes/projectRoute'));
 
